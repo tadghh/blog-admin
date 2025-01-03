75b8e4b0f2181f5adaf68771369e8b9b2eaa82d5
tadghh <thenry.he@gmail.com>
2025-01-02 19:08:47 -0600

    tauri conf fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::{PgPool, PgPoolOptions};
@@ -113,7 +118,6 @@ async fn save_settings(settings: Settings, app: tauri::AppHandle) -> Result<(), 
     .await
     .expect("Failed to write settings");
 
-    println!("Settings saved at: {:?}", settings_path);
     Ok(())
 }
 
@@ -221,6 +225,7 @@ async fn add_tags_to_project(
     }
     Ok(())
 }
+
 #[tauri::command]
 async fn update_blog_tags(
     state: State<'_, Mutex<AppState>>,
@@ -431,6 +436,7 @@ async fn create_project(
     .await
     .map_err(|e| e.to_string())
 }
+
 #[tauri::command]
 async fn update_blog_post(
     state: State<'_, Mutex<AppState>>,
@@ -542,7 +548,7 @@ async fn connect_db(
     connection_config: ConnectionConfig,
 ) -> Result<bool, String> {
     let pool = PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(1)
         .connect(&connection_config.connection_string)
         .await
         .map_err(|e| e.to_string())?;
